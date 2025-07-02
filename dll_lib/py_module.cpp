51854cef16b4e5ed7dd09cda0ceaa801a2cf0ac3
Oleg <haitsynoleg@yandex.by>
2025-07-02 06:46:29 +0300

    Add full CI/CD: DLL + PyQt app

--- dll_lib/py_module.cpp
+++ dll_lib/py_module.cpp
@@ -1,20 +1,23 @@
 #include <pybind11/embed.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/eval.h>
 #include <iostream>
 
 namespace py = pybind11;
-using namespace py::literals;
 
-PYBIND11_EMBEDDED_MODULE(sensor_api, m) {
-    py::module_ embedded = py::module_::import("embedded.sensor_parser");
+PYBIND11_EMBEDDED_MODULE(wt9011lib, m) {
+    py::module_ sensor = py::module_::import("sensor_parser");
+    py::module_ cmds = py::module_::import("sensor_commands");
 
-    m.def("parse", [=](py::bytes data) {
-        auto result = embedded.attr("WT9011Parser").attr("parse")(data);
-        return result;
+    m.def("parse", [sensor](py::bytes data) {
+        return sensor.attr("WT9011Parser").attr("parse")(data);
     });
 
-    m.def("zeroing_cmd", []() {
-        return py::module_::import("embedded.sensor_parser")
-            .attr("WT9011Parser")
-            .attr("build_command_zeroing")();
+    m.def("zeroing_cmd", [sensor]() {
+        return sensor.attr("WT9011Parser").attr("build_command_zeroing")();
+    });
+
+    m.def("calibrate_cmd", [cmds]() {
+        return cmds.attr("WT9011Commands").attr("build_command_calibration")();
     });
 }
